d524270c939c4f4740b618f7137e85098b2a1ee5
elderkai <2498695596@qq.com>
2020-12-18 18:56:16 +0800

    aaa

---
+++ assets/js/ehManage.js
@@ -0,0 +1,20 @@
+//注意：选项卡 依赖 element 模块，否则无法进行功能性操作
+layui.use('element', function(){
+    var element = layui.element;
+    
+    //…
+  });
+  layui.use('laypage', function(){
+    var laypage = layui.laypage;
+    
+    //执行一个laypage实例
+    laypage.render({
+      elem: 'emlayPage' //注意，这里的 test1 是 ID，不用加 # 号
+      ,count: 50 //数据总数，从服务端得到
+    });
+  });
+  // $(document).ready(function(){
+  //   $(".go_pmBulter").click(function(){
+  //       location.href='../../pages/ehManage/pmBulter.html';
+  //   });
+  //   });
---
+++ assets/js/page.js
@@ -0,0 +1,32 @@
+layui.use(['layer', 'form'], function(){
+    var layer = layui.layer
+    ,form = layui.form;
+    
+  });
+  layui.use('element', function(){
+    var element = layui.element;
+    console.log(element);
+    //…
+  });
+  layui.use('carousel', function(){
+    var carousel = layui.carousel;
+    //建造实例
+    carousel.render({
+      elem: '#test1'
+      ,height:"500px"
+      ,interval:3000
+      ,autoplay:true
+      ,width: '100%' //设置容器宽度
+      ,arrow: 'hover' //始终显示箭头
+      //,anim: 'updown' //切换动画方式
+    });
+  });
+  layui.use('laypage', function(){
+    var laypage = layui.laypage;
+    
+    //执行一个laypage实例
+    laypage.render({
+      elem: 'dyPage' //注意，这里的 test1 是 ID，不用加 # 号
+      ,count: 1100 //数据总数，从服务端得到
+    });
+  });
