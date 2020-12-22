05ce0de6ea34dedae1a5b32ad88fff433dd191ff
elderkai <2498695596@qq.com>
2020-12-22 17:43:57 +0800

    d

--- assets/js/ehManage.js
+++ assets/js/ehManage.js
@@ -1,7 +1,7 @@
 //注意：选项卡 依赖 element 模块，否则无法进行功能性操作
 layui.use('element', function(){
     var element = layui.element;
-    
+ 
     //…
   });
   layui.use('laypage', function(){
@@ -11,6 +11,16 @@ layui.use('element', function(){
     laypage.render({
       elem: 'emlayPage' //注意，这里的 test1 是 ID，不用加 # 号
       ,count: 50 //数据总数，从服务端得到
+   
+    });
+    laypage.render({
+      elem: 'dyPage' //注意，这里的 test1 是 ID，不用加 # 号
+      ,count: 100
+      ,layout: ['count','limit', 'prev', 'page', 'next',]
+      ,jump: function(obj){
+        // console.log(obj)
+      }
+   
     });
   });
   // $(document).ready(function(){
