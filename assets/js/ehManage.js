dda900bbd4565e5542c04fa3b343703785858eeb
elderkai <2498695596@qq.com>
2021-01-14 19:35:01 +0800

    d

--- assets/js/ehManage.js
+++ assets/js/ehManage.js
@@ -1,75 +1,131 @@
 //注意：选项卡 依赖 element 模块，否则无法进行功能性操作
-layui.use('element', function(){
-    var element = layui.element;
- 
-    //…
-  });
-  layui.use('laypage', function(){
-    var laypage = layui.laypage;
-    
-    //执行一个laypage实例
-    laypage.render({
-      elem: 'emlayPage' //注意，这里的 test1 是 ID，不用加 # 号
-      ,count: 50 //数据总数，从服务端得到
-   
-    });
-    laypage.render({
-      elem: 'dyPage' //注意，这里的 test1 是 ID，不用加 # 号
-      ,count: 100
-      ,layout: ['count','limit', 'prev', 'page', 'next',]
-      ,jump: function(obj){
-      }
-   
-    });
-  });
+layui.use('element', function () {
+  var element = layui.element;
 
-  window.onscroll=function(){
-    var topScroll =document.documentElement.scrollTop;//滚动的距离,距离顶部的距离
-    var nav = document.getElementById("navBtns");//获取到导航栏id
-    var fixBox = document.getElementById("fixed_box");//获取到导航栏id
-    
-    var leftLength = fixBox.getBoundingClientRect().left; //div离屏幕右边距离（长度）
-    // if(topScroll>=935&&topScroll<2190){
-    //      nav.style.position = 'fixed';
-    //     nav.style.top = '80px';
-    //     nav.style.right = '30px'  
-    // }
-    // else if(topScroll>=2190)
-    // {
-    //   nav.style.position = 'absolute';
-    //   nav.style.top= '2271px';
-    //   nav.style.left= 'auto';
-    // }else{
-    //     nav.style.position = 'absolute';
-    //     nav.style.top= '995px';
-    // }
-    if(topScroll>200){
-      nav.style.position = 'fixed';
-     nav.style.top = '80%';
-     nav.style.right = '30px'  
-     nav.style.display="block"
-}else{
-nav.style.display="none"
-}
-    if(topScroll >=714&&topScroll<1485)
-    {      
-      //当滚动距离大于x时执行下面的东西
-      fixBox.style.position = 'fixed';
-      fixBox.style.top = '80px';
-      fixBox.style.marginLeft = '0';
-      fixBox.style.left = leftLength+"px"
-   
-  }else if(topScroll>=1485)
-  {
-    fixBox.style.position = 'absolute';
-    fixBox.style.top= '771px';
-    fixBox.style.left= 'auto';
+  //…
+});
+layui.use('laypage', function () {
+  var laypage = layui.laypage;
+
+  //执行一个laypage实例
+  laypage.render({
+    elem: 'emlayPage' //注意，这里的 test1 是 ID，不用加 # 号
+      ,
+    count: 50 //数据总数，从服务端得到
+
+  });
+  laypage.render({
+    elem: 'dyPage' //注意，这里的 test1 是 ID，不用加 # 号
+      ,
+    count: 100,
+    layout: ['count', 'limit', 'prev', 'page', 'next', ],
+    jump: function (obj) {}
+
+  });
+});
+$(".tabs-item").click(function(e){
+  let tabs=$(".tabs-item");
+  for(var i=0;i<tabs.length;i++){
+    tabs[i].className="tabs-item"
   }
-  else
-    {
-        fixBox.style.position = 'absolute';
-        fixBox.style.top= '0';
-        fixBox.style.left= 'auto';
+    e.target.className="tabs-item  goHere"
+  for(var i=0;i<tabs.length;i++){
+    if( tabs[i].className=="tabs-item  goHere"){
+      
+     if(i==0){
+    
+      $('html,body').animate({ scrollTop:699+"px" }, 500); 
+     }
+     else{
+      var topScroll = document.documentElement.scrollTop; 
+      let tabTop=document.getElementById("tabsConent"+(i+1)).offsetTop;
+      $("html, body").animate({
+        scrollTop: $("#tabsConent"+(i+1)).offset().top-80 }, {duration: 200,easing: "swing"});
+
+     }
+    
     }
+  }
+})
+function choiseTabs(value){
+  let tabs=$(".tabs-item");
+
+    for(var i=0;i<tabs.length;i++){
+      tabs[i].className="tabs-item"
+    }
+    tabs[value].className="tabs-item  tab_active"
+}
+window.onscroll = function () {
+  var topScroll = document.documentElement.scrollTop; //滚动的距离,距离顶部的距离
+ 
+  var nav = document.getElementById("navBtns"); //获取到导航栏id
+  var fixBox = document.getElementById("fixed_box"); //获取到导航栏id
+  var dwei = document.getElementById("tabsHeaderBar")
+  var tabsHeader = document.getElementById("tabs_header")
+
+  let tabTop1=document.getElementById("tabsConent1").getBoundingClientRect().top-82
+  let tabTop2=document.getElementById("tabsConent2").getBoundingClientRect().top-82
+  let tabTop3=document.getElementById("tabsConent3").getBoundingClientRect().top-82
+  let tabTop4=document.getElementById("tabsConent4").getBoundingClientRect().top-82
+  let tabTop5=document.getElementById("tabsConent5").getBoundingClientRect().top-82
+  let tabTop6=document.getElementById("tabsConent6").getBoundingClientRect().top-82
+  if(tabTop1>0){
+    choiseTabs(0)
+  }
+  else if(tabTop1<=0&&tabTop2>0){
+    choiseTabs(0)
+  }
+  else if(tabTop2<=0&&tabTop3>0){
+    choiseTabs(1)
+  }
+  else if(tabTop3<=0&&tabTop4>0){
+    choiseTabs(2)
+  }
+  else if(tabTop4<=0&&tabTop5>0){
+    choiseTabs(3)
+  }
+  else if(tabTop5<=0&&tabTop6>0){
+    choiseTabs(4)
+  }
+  else if(tabTop6<=0){
+    choiseTabs(5)
+  }
+  if (dwei.getBoundingClientRect().top <= 0) {
+
+    tabsHeader.style.position = 'fixed';
+    tabsHeader.style.top = '0';
+    tabsHeader.style.display = "block"
+  } else {
+    tabsHeader.style.position = 'relative';
+    tabsHeader.style.top = '0';
+    tabsHeader.style.left = 'auto';
+  }
+
+  var leftLength = fixBox.getBoundingClientRect().left; //div离屏幕右边距离（长度）
+
+  if (topScroll > 200) {
+    nav.style.position = 'fixed';
+    nav.style.top = '80%';
+    nav.style.right = '30px'
+    nav.style.display = "block"
+  } else {
+    nav.style.display = "none"
+  }
+  if (topScroll >= 714 && topScroll < 1485) {
+    //当滚动距离大于x时执行下面的东西
+    fixBox.style.position = 'fixed';
+    fixBox.style.top = '80px';
+    fixBox.style.marginLeft = '0';
+    fixBox.style.left = leftLength + "px"
+
+  } else if (topScroll >= 1485) {
+    fixBox.style.position = 'absolute';
+    fixBox.style.top = '771px';
+    fixBox.style.left = 'auto';
+  } else {
+    fixBox.style.position = 'absolute';
+    fixBox.style.top = '0';
+    fixBox.style.left = 'auto';
+  }
 
 }
--- assets/js/pmBulter.js
+++ assets/js/pmBulter.js
@@ -1,24 +1,41 @@
+function choiseTabs(value){
+  let tabs=$(".tabs-item");
 
+    for(var i=0;i<tabs.length;i++){
+      tabs[i].className="tabs-item"
+    }
+    tabs[value].className="tabs-item  tab_active"
+}
+$(".tabs-item").click(function(e){
+  let tabs=$(".tabs-item");
+  for(var i=0;i<tabs.length;i++){
+    tabs[i].className="tabs-item"
+  }
+    e.target.className="tabs-item  goHere"
+  for(var i=0;i<tabs.length;i++){
+    if( tabs[i].className=="tabs-item  goHere"){
+      
+     if(i==0){
+    
+      $('html,body').animate({ scrollTop:720+"px" }, 500); 
+     }
+     else{
+      var topScroll = document.documentElement.scrollTop; 
+      let tabTop=document.getElementById("tabsConent"+(i+1)).offsetTop;
+      $("html, body").animate({
+        scrollTop: $("#tabsConent"+(i+1)).offset().top-80 }, {duration: 200,easing: "swing"});
+
+     }
+    
+    }
+  }
+})
   window.onscroll=function(){
     var topScroll =document.documentElement.scrollTop;//滚动的距离,距离顶部的距离
     var nav = document.getElementById("navBtns");//获取到导航栏id
     var fixBox = document.getElementById("fixed_box");//获取到导航栏id
     var leftLength = fixBox.getBoundingClientRect().left; //div离屏幕右边距离（长度）
     let height = $(".aboutUs")[0].offsetTop-204;
-//     if(topScroll>=905&&topScroll<height){
-//       nav.style.position = 'fixed';
-//      nav.style.top = '80px';
-//      nav.style.right = '30px'  
-//  }
-//  else if(topScroll>=height)
-//  {
-//    nav.style.position = 'absolute';
-//    nav.style.top= height+84+"px";
-//    nav.style.left= 'auto';
-//  }else{
-//      nav.style.position = 'absolute';
-//      nav.style.top= '995px';
-//  }
 if(topScroll>200){
       nav.style.position = 'fixed';
      nav.style.top = '80%';
@@ -27,18 +44,17 @@ if(topScroll>200){
 }else{
 nav.style.display="none"
 }
-
-    if(topScroll >=724&&topScroll<8425)
+    if(topScroll >=724&&topScroll<7970)
     {      
       fixBox.style.position = 'fixed';
       fixBox.style.top = '80px';
       fixBox.style.marginLeft = '0';
       fixBox.style.left = leftLength+"px"
    
-  }else if(topScroll>=8425)
+  }else if(topScroll>=7970)
   {
     fixBox.style.position = 'absolute';
-    fixBox.style.top= '7665px';
+    fixBox.style.top= '7262px';
     fixBox.style.left= 'auto';
   }
   else
@@ -47,4 +63,57 @@ nav.style.display="none"
         fixBox.style.top= '0';
         fixBox.style.left= 'auto';
     }
+    var tabsHeader = document.getElementById("tabs_header")
+    var dwei = document.getElementById("tabsHeaderBar")
+    let tabTop1=document.getElementById("tabsConent1").getBoundingClientRect().top-82
+    let tabTop2=document.getElementById("tabsConent2").getBoundingClientRect().top-82
+    let tabTop3=document.getElementById("tabsConent3").getBoundingClientRect().top-82
+    let tabTop4=document.getElementById("tabsConent4").getBoundingClientRect().top-82
+    let tabTop5=document.getElementById("tabsConent5").getBoundingClientRect().top-82
+    if(tabTop1>0){
+      choiseTabs(0)
+    }
+    else if(tabTop1<=0&&tabTop2>0){
+      choiseTabs(0)
+    }
+    else if(tabTop2<=0&&tabTop3>0){
+      choiseTabs(1)
+    }
+    else if(tabTop3<=0&&tabTop4>0){
+      choiseTabs(2)
+    }
+    else if(tabTop4<=0&&tabTop5>0){
+      choiseTabs(3)
+    }
+    else if(tabTop5<=0){
+      choiseTabs(4)
+    }
+    if (dwei.getBoundingClientRect().top <= 0) {
+
+      tabsHeader.style.position = 'fixed';
+      tabsHeader.style.top = '0';
+      tabsHeader.style.display = "block"
+    } else {
+      tabsHeader.style.position = 'relative';
+      tabsHeader.style.top = '0';
+      tabsHeader.style.left = 'auto';
+    }
 }
+$(".clickItem").click(function(e){
+ let clickItems=document.getElementsByClassName("clickItem")
+ for(var i=0;i<4;i++){
+   clickItems[i].className="clickItem"
+  }
+  e.target.className="clickItem checked_product"
+  for(var i=0;i<4;i++){
+    if( clickItems[i].className=="clickItem checked_product"){
+      if(i==2){}
+      $(".lbNavBody").css("left",-400*i+"px")
+    }
+   }
+})
+$('#btnGoTop').on('click', function() {
+  $('html,body').animate({ scrollTop: '0px' }, 1000); 
+    return false; 
+
+});
